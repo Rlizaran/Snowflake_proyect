b3912410fee9925a433de04d29a870b95f29f78e
R Lizaran <rlizaran97@gmail.com>
2026-05-04 14:59:31 +0000

    fixed data_values tenth of c

--- models/staging/silver/NOAA/slv_weather_daily.sql
+++ models/staging/silver/NOAA/slv_weather_daily.sql
@@ -16,17 +16,17 @@ select
     {{ dbt_utils.generate_surrogate_key(['o.station_id', 'o.observation_date']) }} as daily_id,
 
     -- FKs
-    o.station_id,                                                                             -- -> slv_weather_station
-    o.observation_date,                                                                       -- -> slv_date
+    o.station_id, -- -> slv_weather_station
+    o.observation_date, -- -> slv_date
     s.city,
 
     -- Metricas pivoteadas (todas en grados Celsius o mm, ya escaladas en stg)
-    max(case when o.element_code = 'TMAX' then o.data_value end)                              as temp_max_c,
-    max(case when o.element_code = 'TMIN' then o.data_value end)                              as temp_min_c,
-    max(case when o.element_code = 'TAVG' then o.data_value end)                              as temp_avg_c,
-    max(case when o.element_code = 'PRCP' then o.data_value end)                              as precipitation_mm,
-    max(case when o.element_code = 'SNOW' then o.data_value end)                              as snowfall_mm,
-    max(case when o.element_code = 'SNWD' then o.data_value end)                              as snow_depth_mm,
+    max(case when o.element_code = 'TMAX' then o.data_value end)/10 as temp_max_c,
+    max(case when o.element_code = 'TMIN' then o.data_value end)/10 as temp_min_c,
+    max(case when o.element_code = 'TAVG' then o.data_value end)/10 as temp_avg_c,
+    max(case when o.element_code = 'PRCP' then o.data_value end)/10 as precipitation_mm,
+    max(case when o.element_code = 'SNOW' then o.data_value end) as snowfall_mm,
+    max(case when o.element_code = 'SNWD' then o.data_value end) as snow_depth_mm,
 
     -- Categorizacion derivada para reusar en Gold y filtros PBI
     case
@@ -34,8 +34,8 @@ select
         when max(case when o.element_code = 'SNOW' then o.data_value end) > 0  then 'snowy'
         when max(case when o.element_code = 'TMAX' then o.data_value end) > 25 then 'hot'
         when max(case when o.element_code = 'TMAX' then o.data_value end) < 5  then 'cold'
-        else                                                                        'mild'
-    end                                                                                       as weather_category
+        else 'mild'
+    end as weather_category
 from   obs o
 join   stations s on o.station_id = s.station_id
 group by o.station_id, o.observation_date, s.city
