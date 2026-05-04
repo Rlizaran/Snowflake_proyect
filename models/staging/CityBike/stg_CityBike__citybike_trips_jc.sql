11a73164fc23c022c0c0256fceca754e38472acd
R Lizaran <rlizaran97@gmail.com>
2026-05-04 12:47:18 +0000

    casting

--- models/staging/CityBike/stg_CityBike__citybike_trips_jc.sql
+++ models/staging/CityBike/stg_CityBike__citybike_trips_jc.sql
@@ -4,13 +4,48 @@ with source as (
 
 ),
 
-renamed as (
+casted as (
 
+    select
+        trim(ride_id) as ride_id,
+        lower(trim(rideable_type)) as rideable_type,
+        try_to_timestamp_ntz(started_at) as started_at,
+        try_to_timestamp_ntz(ended_at) as ended_at,
+        trim(start_station_name) as start_station_name,
+        trim(start_station_id) as start_station_id,
+        trim(end_station_name) as end_station_name,
+        trim(end_station_id) as end_station_id,
+        try_to_decimal(start_lat, 10, 6) as start_lat,
+        try_to_decimal(start_lng, 10, 6) as start_lng,
+        try_to_decimal(end_lat, 10, 6) as end_lat,
+        try_to_decimal(end_lng, 10, 6) as end_lng,
+        lower(trim(member_casual)) as member_casual,
+        source_file,
+        load_ts
+
+    from source
+
+),
+
+cleaned as (
+    select
+        *
+    from casted
+    where ride_id is not null
+      and started_at is not null
+      and ended_at is not null
+      and ended_at > started_at
+      and rideable_type in ('classic_bike', 'electric_bike', 'docked_bike', 'rideable_type')
+      and member_casual in ('member', 'casual', 'member_casual')
+),
+
+enriched as (
     select
         ride_id,
         rideable_type,
         started_at,
         ended_at,
+        datediff('minute', started_at, ended_at) as trip_duration_min,
         start_station_name,
         start_station_id,
         end_station_name,
@@ -19,12 +54,12 @@ renamed as (
         start_lng,
         end_lat,
         end_lng,
+        round(st_distance(st_makepoint(start_lng, start_lat), st_makepoint(end_lng,end_lat))/1000.0, 3) as trip_distance_km,
         member_casual,
+        'JC' as city,
         source_file,
         load_ts
-
-    from source
-
+    from cleaned
 )
 
-select * from renamed
+select * from enriched
