ff40e1ce4f1f55615d01c2583763b4002b395761
Rodrigo <rlizaran97@gmail.com>
2026-05-13 21:54:12 +0200

    fixed

--- _python_scripts/us_state_weather.py
+++ _python_scripts/us_state_weather.py
@@ -32,5 +32,5 @@ df_final['state'] = df_final['state'].str.strip()
 df_final['city'] = df_final['station_name'].apply(lambda x: x.split(' ')[0].title())
 
 # 6. Save as CSV for dbt Seed
-df_final.to_csv('../seeds/slv_weather_station.csv', index=False)
-print(f"Success! Created 'slv_weather_station.csv' with {len(df_final)} stations.")
+df_final.to_csv('../seeds/weathweather_station_user_station.csv', index=False)
+print(f"Success! Created 'weather_station_us.csv' with {len(df_final)} stations.")
